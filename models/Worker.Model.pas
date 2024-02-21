5ad393f1284ef55bd4ecfe51aba49692ec86eb64
Giovanna Sousa <giovannasousa.code@gmail.com>
2024-02-21 10:09:30 -0400

    Improved comments and features in the employee model

--- models/Worker.Model.pas
+++ models/Worker.Model.pas
@@ -12,18 +12,18 @@ uses
 type
   TWorker = class
   private
-    FID_WORKER: Integer;
-    FNAME: String;
-    FPASSWORD: String;
-    FIS_ACTIVE: String;
+    FID_WORKER : Integer;
+    FNAME      : String;
+    FPASSWORD  : String;
+    FIS_ACTIVE : String;
 
   public
 
     // Properties to access class members...
-    property ID_WORKER: Integer read FID_WORKER write FID_WORKER;
-    property NAME: String read FNAME write FNAME;
-    property PASSWORD: String read FPASSWORD write FPASSWORD;
-    property IS_ACTIVE: String read FIS_ACTIVE write FIS_ACTIVE;
+    property ID_WORKER : Integer read FID_WORKER write FID_WORKER;
+    property NAME      : String  read FNAME      write FNAME;
+    property PASSWORD  : String  read FPASSWORD  write FPASSWORD;
+    property IS_ACTIVE : String  read FIS_ACTIVE write FIS_ACTIVE;
 
     // Constructor and destructor...
     constructor Create;
@@ -32,8 +32,9 @@ type
     // Methods for CRUD operations...
     function List_Workers(order_by: String; out error: String): TFDQuery;
     function Add_Worker(out error: String): Boolean;
-    function Update_Worker(out error: String): Boolean;
+    function Update_Worker(out error: String): TFDQuery;
     function Delete_Worker(out error: String): Boolean;
+    function Login_Worker(out error : String) : TFDQuery;
   end;
 
 implementation
@@ -66,7 +67,7 @@ begin
         qry.Active := false;
         qry.SQL.Clear;
 
-        // Build SQL query based on parameters...
+        // Build SQL query to get all saved workers...
         qry.SQL.Add('SELECT * FROM worker');
 
         if ID_WORKER > 0 then
@@ -88,13 +89,53 @@ begin
     except on ex: exception do
         begin
             // Handle exceptions and provide an error message...
-            error := 'Error when consulting employees: ' + ex.Message;
+            error := 'Error when consulting employees from the database: ' + ex.Message;
             Result := nil;
         end;
     end;
 end;
 
-// Method to add a new customer to the database...
+// Method to log user into the system
+function TWorker.Login_Worker(out error: String): TFDQuery;
+var
+    qry : TFDQuery;
+begin
+
+    if ID_WORKER < 1 then
+    begin
+        error := 'Enter the employee''s ID';
+        exit;
+    end;
+
+    try
+        // Create and configure a FireDAC query...
+        qry := TFDQuery.Create(nil);
+        qry.Connection := Connection.Model.FConnection;
+
+        qry.Active := false;
+        qry.SQL.Clear;
+
+        // Build a SQL query to get the worker's information from the database...
+        qry.SQL.Add('SELECT * FROM worker');
+        qry.SQL.Add(' WHERE id_worker = :ID_WORKER');
+        qry.SQL.Add(' AND password_worker = :PASSWORD');
+        qry.SQL.Add(' AND is_active = S');
+        qry.ParamByName('ID_WORKER').Value := ID_WORKER;
+        qry.ParamByName('PASSWORD').Value := PASSWORD;
+
+        error := '';
+        Result := qry;
+
+    except on ex:exception do
+        begin
+            // Handle errors and provide an error message...
+            error := 'Error when logging employee: ' + ex.Message;
+            Result := nil;
+        end;
+    end;
+end;
+
+// Method to add a new worker to the database...
 function TWorker.Add_Worker(out error: String): Boolean;
 var
     qry: TFDQuery;
@@ -115,17 +156,33 @@ begin
         qry.Active := false;
         qry.SQL.Clear;
 
+        // Build a SQL query to check if a registration
+        // no longer exists or if it's deactivated...
+        qry.SQL.Add('SELECT * FROM worker');
+        qry.SQL.Add(' WHERE name_worker = :NAME AND is_active = "S"');
+        qry.ParamByName('NAME').Value := NAME;
+
+        if qry.RecordCount > 0 then
+        begin
+            error := 'Error : Worker with name ' + NAME + ' already exists.';
+            raise Exception.Create(error);
+        end;
+
+        qry.SQL.Clear;
+
         // Build SQL query for inserting a new worker...
         qry.SQL.Add('INSERT INTO worker(name_worker, password_worker)');
         qry.SQL.Add(' VALUES(:NAME, :PASSWORD)');
-        qry.ParamByName('NAME').Value := NAME;
+
+        qry.ParamByName('NAME').Value     := NAME;
         qry.ParamByName('PASSWORD').Value := PASSWORD;
 
         qry.ExecSQL;
 
-        // Retrieve the ID of the newly inserted customer...
         qry.Params.Clear;
         qry.SQL.Clear;
+
+        // Retrieve the ID of the newly inserted worker...
         qry.SQL.Add('SELECT MAX(id_worker) AS ID_WORKER FROM worker');
         qry.SQL.Add(' WHERE name_worker = :NAME');
         qry.ParamByName('NAME').Value := NAME;
@@ -146,8 +203,8 @@ begin
     end;
 end;
 
-// Function to update an existing worker...
-function TWorker.Update_Worker(out error: String): Boolean;
+// Method to update an existing worker from the database...
+function TWorker.Update_Worker(out error: String): TFDQuery;
 var
     qry: TFDQuery;
 begin
@@ -155,7 +212,6 @@ begin
     // Validate input data...
     if NAME.IsEmpty then
     begin
-        Result := false;
         error := 'Enter the employee''s name';
         exit;
     end;
@@ -168,26 +224,33 @@ begin
         qry.Active := false;
         qry.SQL.Clear;
 
-        // Build SQL query for updating an existing customer...
+        // Build SQL query for updating an existing worker...
         qry.SQL.Add('UPDATE worker SET name_worker = :NAME,');
         qry.SQL.Add(' password_worker = :PASSWORD, is_active = :IS_ACTIVE');
         qry.SQL.Add(' WHERE id_worker = :ID_WORKER');
-        qry.ParamByName('NAME').Value := NAME;
-        qry.ParamByName('PASSWORD').Value := PASSWORD;
+        qry.ParamByName('NAME').Value      := NAME;
+        qry.ParamByName('PASSWORD').Value  := PASSWORD;
         qry.ParamByName('IS_ACTIVE').Value := IS_ACTIVE;
         qry.ParamByName('ID_WORKER').Value := ID_WORKER;
 
         qry.ExecSQL;
 
-        qry.Free;
+        qry.SQL.Clear;
+        qry.Params.Clear;
+
+        qry.SQL.Add('SELECT * FROM worker WHERE id_worker = :ID_WORKER');
+        qry.ParamByName('ID_WORKER').Value := ID_WORKER;
+
+        qry.Active := true;
+
         error := '';
-        Result := true;
+        Result := qry;
 
     except on ex: exception do
         begin
             // Handle exceptions and provide an error message...
             error := 'Error editing employee: ' + ex.Message;
-            Result := false;
+            Result := nil;
         end;
     end;
 end;
@@ -205,7 +268,7 @@ begin
         qry.Active := false;
         qry.SQL.Clear;
 
-        // Build SQL query based on parameters...
+        // Build SQL query to delete a worker...
         qry.SQL.Add('DELETE FROM worker WHERE id_worker = :ID_WORKER');
         qry.ParamByName('ID_WORKER').Value := ID_WORKER;
 
@@ -218,7 +281,7 @@ begin
     except on ex: exception do
         begin
             // Handle exceptions and provide an error message...
-            error := 'Error deleting employee: ' + ex.Message;
+            error := 'Error when deleting the employee: ' + ex.Message;
             Result := false;
         end;
     end;
