4469bb7e08c3a7de7c40d5bad6795af048987942
Giovanna Sousa <giovannasousa.code@gmail.com>
2024-02-21 10:13:03 -0400

    Improved comments and procedures in the customer controller

--- controllers/Customer.Controller.pas
+++ controllers/Customer.Controller.pas
@@ -20,122 +20,133 @@ implementation
 procedure ListAllCustomers(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
     arrayCustomers : TJSONArray;
-    customer : TCustomer;
-    error : String;
-    qry : TFDQuery;
+    customer       : TCustomer;
+    error          : String;
+    query          : TFDQuery;
 begin
-
-    // Attempt to create a TCustomer instance...
     try
         // Write a separator and log that the request
         // to list all customers has started...
         Writeln('------------------------------');
         Writeln('Request GET - List all customers');
 
+        // Attempt to create a TCustomer instance...
         customer := TCustomer.Create();
+
+        // Create a TFDQuery instance...
+        query := TFDQuery.Create(nil);
+
+        try
+            // Attempt to list all customers...
+            query := customer.List_Customers('', error);
+
+            if error <> '' then
+                raise Exception.Create(error);
+
+            // Convert the result to a JSON array...
+            arrayCustomers := query.ToJSONArray();
+
+            // Send a response with a 200 status code (OK)
+            Res.Send<TJSONArray>(arrayCustomers).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Successfully listed customers - 200');
+
+        finally
+            // Free resources...
+            query.Free;
+            customer.Free
+        end;
+
     except
-        // Handle error during instance creation...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when querying the database - 500');
-        exit;
+        // Handle error during instance creation or listing customers...
+        Res.Send('Error when querying customers from the database: ' + error).Status(500);
+
+        // Log when the operation fails and what caused it...
+        Writeln('Error when querying customers from the database - 500');
+        Writeln('Error Message: ' + error);
     end;
-
-    try
-        // Attempt to list all customers and
-        // convert the result to a JSON array...
-        qry := customer.List_Customers('', error);
-        arrayCustomers := qry.ToJSONArray();
-        Res.Send<TJSONArray>(arrayCustomers);
-
-        // Log the successful completion of the request...
-        Writeln('Successfully listed customers - 200');
-
-    finally
-        // Free resources...
-        qry.Free;
-        customer.Free
-    end;
-
 end;
 
-// List a specific worker by ID...
+// List a specific customer by ID...
 procedure ListCustomerID(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    qry : TFDQuery;
+    qry         : TFDQuery;
     objCustomer : TJSONObject;
-    customer : TCustomer;
-    error : String;
+    customer    : TCustomer;
+    error       : String;
 begin
-
-    // Attempt to create an TCustomer instance...
     try
         // Write a separator and log that the request
-        // to list worker by ID has started...
+        // to list customer by ID has started...
         Writeln('------------------------------');
         Writeln('Request GET - List customer by ID');
 
+        // Attempt to create an TCustomer instance and set its ID_CUSTOMER property
+        // based on the request params...
         customer := TCustomer.Create();
         customer.ID_CUSTOMER := req.Params['id'].ToInteger;
-    except
-        // Handle error during instance creation or ID extraction...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when querying the database - 500');
-        exit;
-    end;
 
-    try
-        // Attempt to list the specified customer
-        // and convert the result to a JSON object...
-        qry := customer.List_Customers('', error);
+        try
+            // Attempt to list the specified customer
+            // and convert the result to a JSON object...
+            qry := customer.List_Customers('', error);
 
-        if qry.RecordCount > 0 then
-        begin
-            objCustomer := qry.ToJSONObject();
-            Res.Send<TJSONObject>(objCustomer).Status(200);
+            if error <> '' then
+                raise Exception.Create(error);
 
-            // Log the successful completion of the request...
-            Writeln('Customer listed successfully - 200');
-        end
-        else
-        begin
-            Res.Send('Customer not found').Status(404);
+            if qry.RecordCount > 0 then
+            begin
+                // Convert the result to a JSON object...
+                objCustomer := qry.ToJSONObject();
 
-            // Error log that caused the operation to fail...
-            Writeln('Customer not found - 404');
+                // Send a response with a 200 status code (OK)...
+                Res.Send<TJSONObject>(objCustomer).Status(200);
+
+                // Log the successful completion of the request...
+                Writeln('Customer listed successfully - 200');
+            end
+            else
+            begin
+                // Send a response with a 404 status code (Not Found)...
+                Res.Send('Customer not found').Status(404);
+
+                // Error log that caused the operation to fail...
+                Writeln('Customer not found - 404');
+            end;
+
+        finally
+            // Free resources...
+            customer.Free;
+            qry.Free;
         end;
 
-    finally
-        // Free resources...
-        customer.Free;
-        qry.Free;
-    end;
+    except
+        // Handle error during instance creation or listing specified customer...
+        Res.Send('Error while querying customer from the database: ' + error).Status(500);
 
+        //Log when the operation fails and what caused it...
+        Writeln('Error while querying customer from the database - 500');
+        Writeln('Error Message: ' + error);
+    end;
 end;
 
 // Add a new customer...
 procedure AddCustomer(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    error : String;
-    customer : TCustomer;
-    body : TJSONValue;
+    error       : String;
+    customer    : TCustomer;
+    body        : TJSONValue;
     objCustomer : TJSONObject;
 begin
-    // Attempt to create a TCustomer instance...
     try
         // Write a separator and log that the request
         // to add new customer has started...
         Writeln('------------------------------');
         Writeln('Request POST - Add new customer');
 
+        // Attempt to create a TCustomer instance...
         customer := TCustomer.Create();
-    except
-        // Handle errors during instance creation...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when querying the database - 500');
-        exit;
-    end;
-
-    try
 
         try
             // Attempt to parse the request body as JSON
@@ -143,12 +154,12 @@ begin
             body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(req.Body),
              0) as TJsonValue;
 
-            customer.NAME := body.GetValue<String>('nameCustomer', '');
-            customer.ADDRESS := body.GetValue<String>('addressCustomer', '');
+            customer.NAME          := body.GetValue<String>('nameCustomer', '');
+            customer.ADDRESS       := body.GetValue<String>('addressCustomer', '');
             customer.NEIGHBOURHOOD := body.GetValue<String>('neighbourhoodCustomer', '');
-            customer.CITY := body.GetValue<String>('cityCustomer', '');
-            customer.TELEPHONE := body.GetValue<String>('telephoneCustomer', '');
-            customer.PHONE := body.GetValue<String>('phoneCustomer');
+            customer.CITY          := body.GetValue<String>('cityCustomer', '');
+            customer.TELEPHONE     := body.GetValue<String>('telephoneCustomer', '');
+            customer.PHONE         := body.GetValue<String>('phoneCustomer');
 
             // Attempt to add the new customer...
             customer.Add_Customer(error);
@@ -158,175 +169,153 @@ begin
             if error <> '' then
                 raise Exception.Create(error);
 
-        except on ex:exception do
-          begin
-              // Handle exceptions during JSON parsing or customer addition...
-              Res.Send(ex.Message).Status(400);
+            // Create a response JSON object with the new customer's ID...
+            objCustomer := TJSONObject.Create;
+            objCustomer.AddPair('idCustomer', customer.ID_CUSTOMER.ToString);
 
-              // Error log that caused the operation to fail...
-              Writeln('Error when registering customer - 400');
-              Writeln('Error Message: ' + ex.Message);
-              exit;
-          end;
+            // Send the response with a 201 status code (Created)...
+            Res.Send<TJSONObject>(objCustomer).Status(201);
+
+            // Log the successful completion of the request...
+            Writeln('Customer successfully created - 201');
+
+        finally
+            // Free resources...
+            customer.Free;
         end;
 
-        // Create a response JSON object with the new customer's ID...
-        objCustomer := TJSONObject.Create;
-        objCustomer.AddPair('idWorker', customer.ID_CUSTOMER.ToString);
+    except
+        // Handle errors during instance creation or adding new customer...
+        Res.Send('Error while adding new customer to the database').Status(500);
 
-        // Send the response with a 201 status code (Created)...
-        Res.Send<TJSONObject>(objCustomer).Status(201);
-
-        // Log the successful completion of the request...
-        Writeln('Customer successfully created - 201');
-
-    finally
-        // Free resources...
-        customer.Free;
+        // Log when the operations fails and what caused it...
+        Writeln('Error while adding new customer to the database - 500');
+        Writeln('Error Message: ' + error);
+        exit;
     end;
 end;
 
 // Update an existing customer...
 procedure UpdateCustomer(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    customer : TCustomer;
-    error : String;
-    body : TJSONValue;
+    customer    : TCustomer;
+    error       : String;
+    body        : TJSONValue;
     objCustomer : TJSONObject;
+    query       : TFDQuery;
 begin
-    // Attempt to create a TCustomer instance...
     try
         // Write a separator and log that the request
-        // to edit customer by ID has started...
+        // to update customer by ID has started...
         Writeln('------------------------------');
         Writeln('Request PUT - Editing customer');
 
+        // Attempt to create a TCustomer instance and set the ID_CUSTOMER property
+        // based on the request params...
         customer := TCustomer.Create;
-    except
-        // Handle errors during instance creation...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when querying the database - 500');
-        exit;
-    end;
+        customer.ID_CUSTOMER := Req.Params['id'].ToInteger;
 
-    try
+        // Create a FireDAC Query instance...
+        query := TFDQuery.Create(nil);
+
         try
-            // Set the ID_CUSTOMER property based on the request params...
-            customer.ID_CUSTOMER := Req.Params['id'].ToInteger;
-
             // Attempt to parse the request body as JSON
-            // and update customer's information...
+            // and extract customer's new information to update...
             body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(req.Body),
              0) as TJsonValue;
 
-             customer.NAME := body.GetValue<String>('nameCustomer', '');
-             customer.ADDRESS := body.GetValue<String>('addressCustomer', '');
-             customer.NEIGHBOURHOOD := body.GetValue<String>('neighbourhoodCustomer', '');
-             customer.CITY := body.GetValue<String>('cityCustomer', '');
-             customer.TELEPHONE := body.GetValue<String>('telephoneCustomer' ,'');
-             customer.PHONE := body.GetValue<String>('phoneCustomer', '');
-             customer.IS_ACTIVE := body.GetValue<String>('isActive', '');
+            customer.NAME          := body.GetValue<String>('nameCustomer', '');
+            customer.ADDRESS       := body.GetValue<String>('addressCustomer', '');
+            customer.NEIGHBOURHOOD := body.GetValue<String>('neighbourhoodCustomer', '');
+            customer.CITY          := body.GetValue<String>('cityCustomer', '');
+            customer.TELEPHONE     := body.GetValue<String>('telephoneCustomer' ,'');
+            customer.PHONE         := body.GetValue<String>('phoneCustomer', '');
+            customer.IS_ACTIVE     := body.GetValue<String>('isActive', '');
 
-             // Attempt to update the existing customer...
-             customer.Update_Custome(error);
+            // Attempt to update the existing customer...
+            query := customer.Update_Custome(error);
 
-             body.Free;
+            body.Free;
 
-             if error <> '' then
+            if error <> '' then
                 raise Exception.Create(error);
 
-        except on ex:exception do
-            begin
-                // Handle exceptions during JSON parsing or customer update...
-                Res.Send(ex.Message).Status(400);
+            // Create a response JSON object with the updated customer information...
+            objCustomer := query.ToJSONObject();
 
-                // Error log that caused the operation to fail...
-                Writeln('Error editing customer - 400');
-                Writeln('Error Message: ' + ex.Message);
-                exit;
-            end;
+            // Send the response with a 200 status code (OK)...
+            Res.Send<TJSONObject>(objCustomer).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Customer successfully updated - 200');
+
+        finally
+            // Free resources...
+            customer.Free;
+            query.Free;
         end;
 
-        // Create a response JSON object with the updated customer information...
-        objCustomer := TJSONObject.Create;
-        objCustomer.AddPair('idCustomer', customer.ID_CUSTOMER.ToString);
-        objCustomer.AddPair('nameCustomer', customer.NAME);
-        objCustomer.AddPair('addressCustomer', customer.ADDRESS);
-        objCustomer.AddPair('neighbourhoodCustomer', customer.NEIGHBOURHOOD);
-        objCustomer.AddPair('cityCustomer', customer.CITY);
-        objCustomer.AddPair('telephoneCustomer', customer.TELEPHONE);
-        objCustomer.AddPair('isActive', customer.IS_ACTIVE);
+    except
+        // Handle errors during instance creation ou customer's update...
+        Res.Send('Error while updating customer information in database: ' + error).Status(500);
 
-        // Send the response with a 200 status code (OK)...
-        Res.Send<TJSONObject>(objCustomer).Status(200);
-
-        // Log the successful completion of the request...
-        Writeln('Customer edited successfully');
-
-    finally
-        // Free resources...
-        customer.Free;
+        // Log when the operation fails and what caused it...
+        Writeln('Error while updating customer information in database - 500');
+        Writeln('Error Message: ' + error);
     end;
 end;
 
 // Delete an existing customer...
 procedure DeleteCustomer(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    customer : TCustomer;
-    error : String;
+    customer    : TCustomer;
+    error       : String;
     objCustomer : TJSONObject;
 begin
-    // Attempt to create a TCustomer instance...
+
     try
         // Write a separator and log that the request
         // to delete customer has started...
         Writeln('------------------------------');
         Writeln('Request DELETE - Delete customer');
 
+        // Attempt to create a TCustomer instance and set the ID_CUSTOMER property
+        // based on the request params...
         customer := TCustomer.Create;
-    except
-        // Handle erros during instance creation...
-        Res.Send('Error when querying to the database').Status(500);
-        Writeln('Error when querying to the database - 500');
-        exit;
-    end;
+        customer.ID_CUSTOMER := Req.Params['id'].ToInteger;
 
-    try
         try
-            // Set the ID_CUSTOMER property based on the request params...
-            customer.ID_CUSTOMER := Req.Params['id'].ToInteger;
-
             // Attempt to delete the existing customer...
             customer.Delete_Customer(error);
 
             if error <> '' then
                 raise Exception.Create(error);
 
-        except on ex:exception do
-            begin
-                // Handle exceptions during customer deletion...
-                Res.Send(ex.Message).Status(400);
+            // Create a response JSON object with the deleted customer's ID...
+            objCustomer := TJSONObject.Create;
+            objCustomer.AddPair('idCustomer', customer.ID_CUSTOMER);
 
-                // Error log that caused the operation to fail...
-                Writeln('Error deleting customer - 400');
-                Writeln('Error Message: ' + ex.Message);
-            end;
+            // Send the response with a 200 status code (OK)...
+            Res.Send<TJSONObject>(objCustomer).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Customer successfully deleted - 200');
+
+        finally
+            // Free resources...
+            customer.Free;
         end;
 
-        // Create a response JSON object with the deleted customer's ID...
-        objCustomer := TJSONObject.Create;
-        objCustomer.AddPair('idCustomer', customer.ID_CUSTOMER);
+    except
+        // Handle erros during instance creation or customer deleting...
+        Res.Send('Error when deleting customer from the database: ' + error).Status(500);
 
-        // Send the response with a 200 status code (OK)...
-        Res.Send<TJSONObject>(objCustomer).Status(200);
-
-        // Log the successful completion of the request...
-        Writeln('Customer successfully deleted - 200');
-
-    finally
-        // Free resources...
-        customer.Free;
+        // Log when the operation fails and what caused it...
+        Writeln('Error when deleting customer from the database - 500');
+        Writeln('Error Message: ' + error);
+        exit;
     end;
+
 end;
 
 // Registering routes for customer-related operations...
