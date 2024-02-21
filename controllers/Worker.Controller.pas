05441e47eed3be494a51979257cfc4ef564443c0
Giovanna Sousa <giovannasousa.code@gmail.com>
2024-02-21 10:12:08 -0400

    Improved comments and procedures in the worker controller

--- controllers/Worker.Controller.pas
+++ controllers/Worker.Controller.pas
@@ -19,130 +19,147 @@ implementation
 // List all workers...
 procedure ListAllWorkers(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    worker : TWorker;
-    qry : TFDQuery;
-    error : String;
+    worker       : TWorker;
+    query        : TFDQuery;
+    error        : String;
     arrayWorkers : TJSONArray;
 begin
 
-    // Attempt to create a TWorker instance...
     try
         // Write a separator and log that the request
         // to list all workers has started...
         Writeln('------------------------------');
         Writeln('Request GET - List all workers');
 
+        // Attempt to create a TWorker instance...
         worker := TWorker.Create();
+
+        // Create a FireDAC query...
+        query := TFDQuery.Create(nil);
+
+        try
+            // Attempt to list all workers...
+            query := worker.List_Workers('', error);
+
+            if error <> '' then
+                raise Exception.Create(error);
+
+            // Convert the result to a JSON array...
+            arrayWorkers := query.ToJSONArray();
+
+            // Send a response with a 200 status code (OK)...
+            Res.Send<TJSONArray>(arrayWorkers).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Successfully listed workers - 200');
+
+        finally
+            // Free resources...
+            query.Free;
+            worker.Free;
+        end;
+
     except
-        // Handle errors during instance creation...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when quering the database - 500');
-        exit;
+        // Handle errors during instance creation or listing workers...
+        Res.Send('Error when querying workers from the database: ' + error).Status(500);
+
+        // Log when the operation fails and what caused it
+        Writeln('Error when quering workers from the database - 500');
+        Writeln('Error Message: ' + error);
     end;
-
-    try
-        // Attempt to list all workers and
-        //convert the result to a JSON array...
-        qry := worker.List_Workers('', error);
-        arrayWorkers := qry.ToJSONArray();
-        Res.Send<TJSONArray>(arrayWorkers);
-
-        // Log the successful completion of the request...
-        Writeln('Successfully listed workers - 200');
-
-    finally
-        // Free resources...
-        qry.Free;
-        worker.Free;
-    end;
-
 end;
 
 // List a specific worker by ID...
 procedure ListWorkersID(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    qry : TFDQuery;
-    worker : TWorker;
+    query     : TFDQuery;
+    worker    : TWorker;
     objWorker : TJSONObject;
-    error : String;
+    error     : String;
 begin
 
-    // Attempt to create a TWorker instance and set its ID_WORKER property...
     try
         // Write a separator and log that the request
         // to list worker by ID has started...
         Writeln('------------------------------');
         Writeln('Request GET - List worker by ID');
 
+        // Attempt to create a TWorker instance and set its ID_WORKER property
+        // based on the request params...
         worker := TWorker.Create;
         worker.ID_WORKER := req.Params['id'].ToInteger;
-    except
-        // Handle errors during instance creation or ID extraction...
-        res.Send('Error when querying the database').Status(500);
-        Writeln('Error when quering the database - 500');
-        exit;
-    end;
 
-    try
-        // Attempt to list the specified worker
-        // and convert the result to a JSON object...
-        qry := worker.List_Workers('', error);
+        // Create a FireDAC Query...
+        query := TFDQuery.Create(nil);
 
-        if qry.RecordCount > 0 then
-        begin
-            objWorker := qry.ToJSONObject;
-            res.Send<TJSONObject>(objWorker);
+        try
+            // Attempt to list the specified worker...
+            query := worker.List_Workers('', error);
 
-            // Log the successful completion of the request...
-            Writeln('Worker listed by ID successfully - 200');
-        end
-        else
-        begin
-            res.Send('Worker not found').Status(404);
+            if error <> '' then
+                raise Exception.Create(error);
 
-            // Error log that caused the operation to fail...
-            Writeln('Worker not found - 404');
+            if query.RecordCount > 0 then
+            begin
+                // Convert the result to a JSON object...
+                objWorker := query.ToJSONObject();
+
+                // Send a response with a 200 status code (OK)...
+                res.Send<TJSONObject>(objWorker).Status(200);
+
+                // Log the successful completion of the request...
+                Writeln('Worker listed by ID successfully - 200');
+            end
+            else
+            begin
+                // Send a response with a 404 status code (Not found)...
+                res.Send('Worker not found').Status(404);
+
+                // Log that the worker was not found...
+                Writeln('Worker not found - 404');
+            end;
+
+        finally
+            // Free resources...
+            query.Free;
+            worker.Free;
         end;
 
-    finally
-        // Free resources...
-        qry.Free;
-        worker.Free;
-    end;
+    except
+        // Handle errors during instance creation or listing specified worker...
+        res.Send('Error while querying worker from the database: ' + error).Status(500);
 
+        // Log when the operation fails and what caused it...
+        Writeln('Error when quering worker from the database - 500');
+        Writeln('Error Message: ' + error);
+    end;
 end;
 
 // Add a new worker...
 procedure AddWorker(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
-    worker : TWorker;
-    body : TJSONValue;
+    worker    : TWorker;
+    body      : TJSONValue;
     objWorker : TJSONObject;
-    error : String;
+    error     : String;
 begin
-    // Attempt to create a TWorker instance...
+
     try
         // Write a separator and log that the request
         // to add new worker has started...
         Writeln('------------------------------');
         Writeln('Request POST - Add new worker');
 
+        // Attempt to create a TWorker instance...
         worker := TWorker.Create;
-    except
-        // Handle errors during instance creation...
-        res.Send('Error when querying the database').Status(500);
-        Writeln('Error when quering the database - 500');
-        exit;
-    end;
 
-    try
         try
             // Attempt to parse the request body as JSON
             //  and extract worker's information...
             body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(req.Body),
              0) as TJsonValue;
 
-            worker.NAME := body.GetValue<String>('nameWorker', '');
+            worker.NAME     := body.GetValue<String>('nameWorker', '');
             worker.PASSWORD := body.GetValue<String>('passwordWorker', '');
 
             // Attempt to add the new worker...
@@ -153,180 +170,230 @@ begin
             if error <> '' then
                 raise Exception.Create(error);
 
-        except on ex:exception do
-            begin
-                // Handle exceptions during JSON parsing or worker addition...
-                res.Send(ex.Message).Status(400);
+            // Create a response JSON object with the new worker's ID...
+            objWorker := TJSONObject.Create;
+            objWorker.AddPair('idWorker', worker.ID_WORKER.ToString);
 
-                // Error log that caused the operation to fail...
-                Writeln('Error when registering worker - 400');
-                Writeln('Error Message: ' + ex.Message);
-                exit;
-            end;
+            // Send the response with a 201 status code (Created)...
+            Res.Send<TJSONObject>(objWorker).Status(201);
+
+            // Log the successful completion of the request...
+            Writeln('Worker successfully created - 200');
+
+        finally
+            // Free resources...
+            worker.Free;
         end;
 
-        // Create a response JSON object with the new worker's ID...
-        objWorker := TJSONObject.Create;
-        objWorker.AddPair('idWorker', worker.ID_WORKER.ToString);
+    except
+        // Handle errors during instance creation or adding new worker...
+        res.Send('Error while adding new worker to the database: ' + error).Status(500);
 
-        // Send the response with a 201 status code (Created)...
-        res.Send<TJSONObject>(objWorker).Status(201);
-
-        // Log the successful completion of the request...
-        Writeln('Worker successfully created - 200');
-
-    finally
-        // Free resources...
-        worker.Free;
+        // Log when the operation fails and what caused it...
+        Writeln('Error while adding new worker to the database - 500');
+        Writeln('Error Message: ' + error);
     end;
+end;
 
+// Employee login to the system
+procedure LoginWorker(Req : THorseRequest; Res : THorseResponse; Next : TProc);
+var
+    query     : TFDQuery;
+    objWorker : TJSONObject;
+    error     : String;
+    worker    : TWorker;
+    body      : TJSONValue;
+begin
+    try
+        // Write a separator and log that the request
+        // to login worker has started...
+        Writeln('------------------------------');
+        Writeln('Request POST - Login Worker');
+
+        // Attempt to create a TWorker instance...
+        worker := TWorker.Create;
+
+        // Create a FireDAC Query...
+        query := TFDQuery.Create(nil);
+
+        try
+            // Attempt to parse the request body as JSON
+            // and extract worker's information...
+            body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(Req.Body),
+            0) as TJsonValue;
+
+            worker.ID_WORKER := body.GetValue<String>('idWorker', '').ToInteger;
+            worker.PASSWORD  := body.GetValue<String>('passwordWorker', '');
+
+            // Attempt to login with the worker's information...
+            query := worker.Login_Worker(error);
+
+            if error <> '' then
+            begin
+                // Send a response with a 401 status code (Unauthorized)...
+                Res.Send('Error: ' + error).Status(401);
+
+                // Log when the operations fails and what caused it...
+                Writeln('Error when logging worker - 401');
+                Writeln('Error Message: ' + error);
+                exit;
+            end;
+
+            // Create a response JSON Object with the worker's information...
+            objWorker := query.ToJSONObject();
+
+            // Send the response with a 200 status code (OK)...
+            Res.Send<TJSONObject>(objWorker).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Worker successfully logged - 200');
+
+        finally
+            // Free resources...
+            worker.Free;
+            query.Free;
+        end;
+
+    except
+        // Handle errors during instance creation or worker's login...
+        Res.Send('Error when loging worker: ' + error).Status(500);
+
+        // Log when the operation fails and what caused it...
+        Writeln('Error when loging worker - 500');
+        Writeln('Error Message: ' + error);
+    end;
 end;
 
 // Update an existing worker...
 procedure UpdateWorker(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
     objWorker : TJSONObject;
-    error : String;
-    body : TJSONValue;
-    worker : TWorker;
+    error     : String;
+    body      : TJSONValue;
+    worker    : TWorker;
+    query     : TFDQuery;
 begin
 
     try
-        // Attempt to create a TWorker instance...
+        // Write a separator and log that the request
+        // to update worker has started...
         Writeln('------------------------------');
         Writeln('Request PUT - Editing worker');
+
+        // Attempt to create a TWorker instance and set the ID_WORKER property based
+        // on the request params...
         worker := TWorker.Create;
-    except
-        // Handle errors during instance creation...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when querying the database - 500');
-        exit;
-    end;
+        worker.ID_WORKER := Req.Params['id'].ToInteger;
 
-    try
+        // Create a FireDAC Query instance...
+        query := tFDQUery.Create(nil);
+
         try
-            // Set the ID_WORKER property based on the request parameter...
-            worker.ID_WORKER := Req.Params['id'].ToInteger;
-
             // Attempt to parse the request body as JSON
-            // and extract update worker information...
+            // and extract worker's new information to update...
             body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(req.Body),
              0) as TJsonValue;
 
-            worker.NAME := body.GetValue<String>('nameWorker', '');
-            worker.PASSWORD := body.GetValue<String>('passwordWorker', '');
+            worker.NAME      := body.GetValue<String>('nameWorker', '');
+            worker.PASSWORD  := body.GetValue<String>('passwordWorker', '');
             worker.IS_ACTIVE := body.GetValue<String>('isActive', '');
 
             // Attempt to update the existing worker...
-            worker.Update_Worker(error);
+            query := worker.Update_Worker(error);
 
             body.Free;
 
             if error <> '' then
                 raise Exception.Create(error);
 
-        except on ex:exception do
-            begin
-                // Handle exceptions during JSON parsing or worker update...
-                Res.Send(ex.Message).Status(400);
+            // Create a response JSON object with the updated worker information...
+            objWorker := query.ToJSONObject();
 
-                // Error log that caused the operation to fail...
-                Writeln('Error editing worker - 400');
-                Writeln('Error Message: ' + ex.Message);
-                exit;
-            end;
+            // Send the response with a 200 status code (OK)...
+            Res.Send<TJSONObject>(objWorker).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Worker successfully updated - 200');
+
+        finally
+            // Free resources
+            worker.Free;
+            query.Free;
         end;
 
-        // Create a response JSON object with the updated worker information...
-        objWorker := TJSONObject.Create;
-        objWorker.AddPair('idWorker', worker.ID_WORKER.ToString);
-        objWorker.AddPair('nameWorker', worker.NAME);
-        objWorker.AddPair('passwordWorker', worker.PASSWORD);
-        objWorker.AddPair('isActive', worker.IS_ACTIVE);
+    except on ex:exception do
+        begin
+            // Handle errors during instance creation or updating worker...
+            Res.Send('Error while updanting worker information in database: ' + error).Status(500);
 
-        // Send the response with a 200 status code (OK)...
-        Res.Send<TJSONObject>(objWorker).Status(200);
-
-        // Log the successful completion of the request...
-        Writeln('Worker edited successfully - 200');
-
-    finally
-        // Free resources...
-        worker.Free;
+            // Log when the operation fails and what caused it...
+            Writeln('Error while updating worker information in database - 500');
+            Writeln('Error Message: ' + error);
+        end;
     end;
-
 end;
 
 // Delete an existing worker...
 procedure DeleteWorker(Req : THorseRequest; Res : THorseResponse; Next : TProc);
 var
     objWorker : TJSONObject;
-    error : String;
-    worker : TWorker;
+    error     : String;
+    worker    : TWorker;
 begin
 
-    // Attempt to create a TWorker instance...
     try
         // Write a separator and log that the request
         // to delete worker has started...
         Writeln('------------------------------');
         Writeln('Request DELETE - Delete worker');
 
+        // Attempt to create a TWorker instance and set the ID_WORKER property
+        // based on the request params...
         worker := TWorker.Create;
-    except
-        // Handle errors during instance creation...
-        Res.Send('Error when querying the database').Status(500);
-        Writeln('Error when quering the database - 500');
-        exit;
-    end;
+        worker.ID_WORKER := Req.Params['id'].ToInteger;
 
-    try
         try
-            // Set the ID_WORKER property based on the request parameter...
-            worker.ID_WORKER := Req.Params['id'].ToInteger;
-
             // Attempt to delete the existing worker...
             worker.Delete_Worker(error);
 
             if error <> '' then
                 raise Exception.Create(error);
 
-        except on ex:exception do
-            begin
-                // Handle exceptions during worker deletion...
-                Res.Send(ex.Message).Status(400);
+            // Create a response JSON object with the deleted worker's ID...
+            objWorker := TJSONObject.Create;
+            objWorker.AddPair('idWorker', worker.ID_WORKER.ToString);
 
-                // Error log that caused the operation to fail...
-                Writeln('Error deleting worker - 400');
-                Writeln('Error Message: ' + ex.Message);
-            end;
+            // Send the response with a 200 status code (OK)...
+            Res.Send<TJSONObject>(objWorker).Status(200);
+
+            // Log the successful completion of the request...
+            Writeln('Worker successfully deleted - 200');
+
+        finally
+            // Free resources...
+            worker.Free;
         end;
 
-        // Create a response JSON object with the deleted worker's ID...
-        objWorker := TJSONObject.Create;
-        objWorker.AddPair('idWorker', worker.ID_WORKER.ToString);
+    except
+        // Handle errors during instance creation or deleting worker...
+        Res.Send('Error when deleting worker from the database').Status(500);
 
-        // Send the response with a 200 status code (OK)...
-        Res.Send<TJSONObject>(objWorker).Status(200);
-
-        // Log the successful completion of the request...
-        Writeln('Worker successfully deleted - 200');
-
-    finally
-        // Free resources...
-        worker.Free;
+        // Log errors during instance creation or deleting worker...
+        Writeln('Error when deleting worker from the database - 500');
+        Writeln('Error Message: ' + error);
     end;
-
 end;
 
 // Register routes for worker-related operations...
 procedure Registry;
 begin
-    THorse.Get('/workers', ListAllWorkers);
-    THorse.Get('/workers/:id', ListWorkersID);
-    THorse.Post('/workers', AddWorker);
-    THorse.Put('/workers/:id', UpdateWorker);
-    THorse.Delete('/workers/:id', DeleteWorker);
+    THorse
+        .Get('/workers', ListAllWorkers)
+        .Get('/workers/:id', ListWorkersID)
+        .Post('/workers', AddWorker)
+        .Post('/workers/login', LoginWorker)
+        .Put('/workers/:id', UpdateWorker)
+        .Delete('/workers/:id', DeleteWorker);
 end;
 
 end.
