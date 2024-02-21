ee04bdbc9d8f634dc6711ac9fddbf6d062d96e57
Giovanna Sousa <giovannasousa.code@gmail.com>
2024-02-21 10:10:33 -0400

    Improved comments and features in the customer model

--- models/Customer.Model.pas
+++ models/Customer.Model.pas
@@ -12,26 +12,26 @@ uses
 type
     TCustomer = class
     private
-        FID_CUSTOMER : Integer;
-        FNAME : String;
-        FADDRESS : String;
+        FID_CUSTOMER   : Integer;
+        FNAME          : String;
+        FADDRESS       : String;
         FNEIGHBOURHOOD : String;
-        FCITY : String;
-        FTELEPHONE : String;
-        FPHONE : String;
-        FIS_ACTIVE : String;
+        FCITY          : String;
+        FTELEPHONE     : String;
+        FPHONE         : String;
+        FIS_ACTIVE     : String;
 
     public
 
         // Properties for accessing private fields...
-        property ID_CUSTOMER : Integer read FID_CUSTOMER write FID_CUSTOMER;
-        property NAME : String read FNAME write FNAME;
-        property ADDRESS : String read FADDRESS write FADDRESS;
-        property NEIGHBOURHOOD : String read FNEIGHBOURHOOD write FNEIGHBOURHOOD;
-        property CITY : String read FCITY write FCITY;
-        property TELEPHONE : String read FTELEPHONE write FTELEPHONE;
-        property PHONE : String read FPHONE write FPHONE;
-        property IS_ACTIVE : String read FIS_ACTIVE write FIS_ACTIVE;
+        property ID_CUSTOMER   : Integer read FID_CUSTOMER   write FID_CUSTOMER;
+        property NAME          : String  read FNAME          write FNAME;
+        property ADDRESS       : String  read FADDRESS       write FADDRESS;
+        property NEIGHBOURHOOD : String  read FNEIGHBOURHOOD write FNEIGHBOURHOOD;
+        property CITY          : String  read FCITY          write FCITY;
+        property TELEPHONE     : String  read FTELEPHONE     write FTELEPHONE;
+        property PHONE         : String  read FPHONE         write FPHONE;
+        property IS_ACTIVE     : String  read FIS_ACTIVE     write FIS_ACTIVE;
 
         // Constructor and destructor...
         constructor Create;
@@ -40,7 +40,7 @@ type
         // Methods for CRUD operations...
         function List_Customers(order_by : String; out error : String) : TFDQuery;
         function Add_Customer(out error : String) : Boolean;
-        function Update_Custome(out error : String) : Boolean;
+        function Update_Custome(out error : String) : TFDQuery;
         function Delete_Customer(out error : String) : Boolean;
 end;
 
@@ -62,8 +62,7 @@ begin
 end;
 
 // Method to retrieve a list of all or a specific customer from the database...
-function TCustomer.List_Customers(order_by: String;
-  out error: String): TFDQuery;
+function TCustomer.List_Customers(order_by: String; out error: String): TFDQuery;
 var
     qry : TFDQuery;
 begin
@@ -75,7 +74,7 @@ begin
         qry.Active := false;
         qry.SQL.Clear;
 
-        // Build SQL query based on parameters...
+        // Build SQL query to get all saved customers...
         qry.SQL.Add('SELECT * FROM customer');
 
         if ID_CUSTOMER > 0 then
@@ -97,7 +96,7 @@ begin
     except on ex:exception do
         begin
             // Handle exceptions and provide an error message...
-            error := 'Error when querying customers: ' + ex.Message;
+            error := 'Error when consulting customers from the database: ' + ex.Message;
             Result := nil;
         end;
     end;
@@ -144,21 +143,24 @@ begin
 
         // Build SQL query for inserting a new customer...
         qry.SQL.Add('INSERT INTO customer(name_customer, address_customer,');
-        qry.SQL.Add(' neighbourhood_customer, city_customer, telephone_customer,');
-        qry.SQL.Add(' phone_customer)');
-        qry.SQL.Add(' VALUES (:NAME, :ADDRESS, :NEIGHBOURHOOD, :CITY, :TELEPHONE, :PHONE)');
-        qry.ParamByName('NAME').Value := NAME;
-        qry.ParamByName('ADDRESS').Value := ADDRESS;
+        qry.SQL.Add(' neighbourhood_customer, city_customer,');
+        qry.SQL.Add(' telephone_customer, phone_customer)');
+        qry.SQL.Add(' VALUES (:NAME, :ADDRESS, :NEIGHBOURHOOD, :CITY,');
+        qry.SQL.Add(' :TELEPHONE, :PHONE)');
+
+        qry.ParamByName('NAME').Value          := NAME;
+        qry.ParamByName('ADDRESS').Value       := ADDRESS;
         qry.ParamByName('NEIGHBOURHOOD').Value := NEIGHBOURHOOD;
-        qry.ParamByName('CITY').Value := CITY;
-        qry.ParamByName('TELEPHONE').Value := TELEPHONE;
-        qry.ParamByName('PHONE').Value := PHONE;
+        qry.ParamByName('CITY').Value          := CITY;
+        qry.ParamByName('TELEPHONE').Value     := TELEPHONE;
+        qry.ParamByName('PHONE').Value         := PHONE;
 
         qry.ExecSQL;
 
-        // Retrieve the ID of the newly inserted customer...
         qry.Params.Clear;
         qry.SQL.Clear;
+
+        // Retrieve the ID of the newly inserted customer...
         qry.SQL.Add('SELECT MAX(id_customer) AS ID_CUSTOMER FROM customer');
         qry.SQL.Add(' WHERE name_customer = :NAME');
         qry.ParamByName('NAME').Value := NAME;
@@ -180,8 +182,8 @@ begin
     end;
 end;
 
-// Method to update an existing customer in the database...
-function TCustomer.Update_Custome(out error: String): Boolean;
+// Method to update an existing customer from the database...
+function TCustomer.Update_Custome(out error: String): TFDQuery;
 var
     qry : TFDQuery;
 begin
@@ -229,26 +231,33 @@ begin
         qry.SQL.Add(' is_active = :IS_ACTIVE');
         qry.SQL.Add(' WHERE id_customer = :ID_CUSTOMER');
 
-        qry.ParamByName('NAME').Value := NAME;
-        qry.ParamByName('ADDRESS').Value := ADDRESS;
+        qry.ParamByName('NAME').Value          := NAME;
+        qry.ParamByName('ADDRESS').Value       := ADDRESS;
         qry.ParamByName('NEIGHBOURHOOD').Value := NEIGHBOURHOOD;
-        qry.ParamByName('CITY').Value := CITY;
-        qry.ParamByName('TELEPHONE').Value := TELEPHONE;
-        qry.ParamByName('PHONE').Value := PHONE;
-        qry.ParamByName('IS_ACTIVE').Value := IS_ACTIVE;
-        qry.ParamByName('ID_CUSTOMER').Value := ID_CUSTOMER;
+        qry.ParamByName('CITY').Value          := CITY;
+        qry.ParamByName('TELEPHONE').Value     := TELEPHONE;
+        qry.ParamByName('PHONE').Value         := PHONE;
+        qry.ParamByName('IS_ACTIVE').Value     := IS_ACTIVE;
+        qry.ParamByName('ID_CUSTOMER').Value   := ID_CUSTOMER;
 
         qry.ExecSQL;
 
-        qry.Free;
+        qry.SQL.Clear;
+        qry.Params.Clear;
+
+        qry.SQL.Add('SELECT * FROM customer WHERE id_customer = :ID_CUSTOMER');
+        qry.ParamByName('ID_CUSTOMER').Value := ID_CUSTOMER;
+
+        qry.Active := true;
+
         error := '';
-        Result := true;
+        Result := qry;
 
     except on ex:exception do
         begin
             // Handle exceptions and provide an error message...
             error := 'Error when editing the customer: ' + ex.Message;
-            Result := false;
+            Result := nil;
         end;
     end;
 end;
